ad8382a8b54e79dd71339452fac5035c9c19c8be
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-07-21 09:33:24 +0530

    Add files via upload

---
+++ Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LambdaLinq_Demo
+{
+	public class Management
+	{
+		public void TopRecord(List<ProductReview> products)
+		{
+			var result = products.OrderByDescending(x => x.Rating).Take(3);
+			var record = (from product in products orderby product.Review descending select product);
+			Console.WriteLine("using LinQ");
+			Console.WriteLine("-----------");
+			foreach(var prod in record)
+			{
+				Console.WriteLine(prod.ProductId + " " + prod.UserId + " " + prod.Rating + " " + prod.Review + " " + prod.IsLike);
+			}
+			foreach(var prod in result)
+			{
+				Console.WriteLine(prod.ProductId+" "+ prod.UserId+" "+ prod.Rating+ " "+ prod.Review+" "+ prod.IsLike);
+			}
+		}
+	}
+}
+
---
+++ ProductReview.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace LambdaLinq_Demo
+{
+	//Lambda expression is used to create anonymous function, expression and
+	//body memeber.
+	//It's a short block of code that accepts parameter and return a value
+	//Use => is a lambda operatorwhich is used for all the lambda expressions.
+	//Linq= Its a language integerated query
+	public class ProductReview
+	{
+		public int ProductId { get; set; }
+		public int UserId { get; set; }
+		public int Rating { get; set; }
+		public string Review { get; set; }
+		public bool IsLike { get; set; }
+		
+	}
+
+}
